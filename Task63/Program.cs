4d64e2ab97c192208b2361634382a4e31691e12f
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-28 02:12:04 +0300

    Task63 - с методом создания int

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -4,14 +4,22 @@
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-NaturalNumbers(number);
+int CreateNumber(string msg)    // юзер вводит число
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-void NaturalNumbers(int num)
+void NaturalNumbers(int num)    // вывод натуральных чисел от 1 до N
 {
     if (num == 0) return;
     NaturalNumbers(num - 1);
     Console.Write($"{num} ");
 }
 
+Console.Clear();
+Console.WriteLine("Вывод натуральных чисел в промежутке от 1 до N.\n"
+                + "-----------------------------------------------");
+
+int number = CreateNumber("Введите число N: ");
+NaturalNumbers(number);
